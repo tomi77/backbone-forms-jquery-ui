cdc3d29be7035ee879dd7cce164eff6d353fc856
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2018-01-09 20:25:47 +0100

    split checkboxradio demo to checkbox, checkboxes and radio demos

---
+++ docs/checkbox-demo.js
@@ -0,0 +1,27 @@
+var loaded = false;
+var example = function () {
+  if(loaded) return;
+  loaded = true;
+  var form = new Backbone.Form({
+    schema: {
+      val: {
+        type: 'jqueryui.checkbox',
+        title: 'Val'
+      }
+    }
+  });
+  var editor = form.getEditor('val');
+  editor.on('change', function(editor) {
+    $('.change span').html(editor.getValue());
+  }).on('focus', function(editor) {
+    $('.focus').removeClass('hidden');
+    $('.blur').addClass('hidden');
+  }).on('blur', function(editor) {
+    $('.focus').addClass('hidden');
+    $('.blur').removeClass('hidden');
+  });
+  $('#form').html(form.render().$el);
+  editor.trigger('blur', editor);
+};
+document.addEventListener("DOMContentLoaded", example, false);
+window.addEventListener("load", example, false);
---
+++ docs/radio-demo.js
@@ -0,0 +1,28 @@
+var loaded = false;
+var example = function () {
+  if(loaded) return;
+  loaded = true;
+  var form = new Backbone.Form({
+    schema: {
+      val: {
+        type: 'jqueryui.radio',
+        title: 'Val',
+        options: ['1', '2', '3']
+      }
+    }
+  });
+  var editor = form.getEditor('val');
+  editor.on('change', function(editor) {
+    $('.change span').html(editor.getValue());
+  }).on('focus', function(editor) {
+    $('.focus').removeClass('hidden');
+    $('.blur').addClass('hidden');
+  }).on('blur', function(editor) {
+    $('.focus').addClass('hidden');
+    $('.blur').removeClass('hidden');
+  });
+  $('#form').html(form.render().$el);
+  editor.trigger('blur', editor);
+};
+document.addEventListener("DOMContentLoaded", example, false);
+window.addEventListener("load", example, false);
