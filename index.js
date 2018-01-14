961b04118e03e4277a4ed4e39c328850f3a7ee37
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2018-01-14 16:50:43 +0100

    added nodejs entry file

---
+++ index.js
@@ -0,0 +1,25 @@
+
+/*
+  Backbone-Forms jQuery UI editors 0.1.0
+
+  Copyright (c) 2016 Tomasz Jakub Rup
+
+  https://github.com/tomi77/backbone-forms-jquery-ui
+
+  Released under the MIT license
+ */
+require('./autocomplete');
+
+require('./checkbox');
+
+require('./checkboxes');
+
+require('./datepicker');
+
+require('./radio');
+
+require('./selectmenu');
+
+require('./slider');
+
+require('./spinner');
